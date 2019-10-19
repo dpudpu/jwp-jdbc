2a420bdd86478ae888de1f77c006495f017f8b45
DaejunBae <dpudpu11@gmail.com>
2019-10-19 19:46:13 +0900

    refactor: RuntimeSQLException -> DataAccessException, 이름, 상위클래스 변꼉

--- nextstep-jdbc/src/main/java/nextstep/jdbc/DataAccessException.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/DataAccessException.java
@@ -2,7 +2,7 @@ package nextstep.jdbc;
 
 import java.sql.SQLException;
 
-public class DataAccessException extends RuntimeException {
+public class DataAccessException extends SQLException {
 
     public DataAccessException(final SQLException exception) {
         super(exception);
